d6d4e0d03dc8d22d45393732360ff086e9aff2ed
underscoDe <developer.awounou@gmail.com>
2021-09-13 05:47:46 +0100

    make index redirects to login page

--- index.php
+++ index.php
@@ -1,53 +1,7 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-    <title>My Awesome Login Page</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/main.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-</head>
-<!--Coded with love by Mutiullah Samim-->
-<body>
-    <div class="container h-100">
-        <div class="d-flex justify-content-center h-100">
-            <div class="user_card">
-                <div class="d-flex justify-content-center">
-                    <div class="brand_logo_container">
-                        <img src="images/mapcom.jpg" class="brand_logo" alt="Logo">
-                    </div>
-                </div>
-                <div class="d-flex justify-content-center form_container">
-                    <form>
-                        <div class="input-group mb-3">
-                            <div class="input-group-append">
-                                <span class="input-group-text"><i class="fas fa-user"></i></span>
-                            </div>
-                            <input type="text" name="" class="form-control input_user" value="" placeholder="Identifiant">
-                        </div>
-                        <div class="input-group mb-2">
-                            <div class="input-group-append">
-                                <span class="input-group-text"><i class="fas fa-key"></i></span>
-                            </div>
-                            <input type="password" name="" class="form-control input_pass" placeholder="Mot de passe">
-                        </div>
-                        <div class="d-flex justify-content-center mt-3 px-0 login_container">
-                            <button type="button" name="button" class="btn login_btn">Login</button>
-                        </div>
-                    </form>
-                </div>
+// index.php redirects to login
+header("Location: login.php");
+exit();
 
-                <div class="mt-4">
-                    <div class="d-flex justify-content-center links">
-                        Pas de compte ? <a href="#" class="ml-2">Inscrivez-vous !</a>
-                    </div>
-                    <div class="d-flex justify-content-center links">
-                        <a href="#">Forgot your password?</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-</html>
+?>
