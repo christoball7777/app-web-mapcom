205783ddb26efdd6c14ea449bf464eda7a3cad96
underscoDe <developer.awounou@gmail.com>
2021-09-15 14:02:28 +0100

    add tab panels

--- dashboard.php
+++ dashboard.php
@@ -3,10 +3,12 @@ require_once 'database/connection.php';
 
 try {
 
-    $sql = 'SELECT * FROM user';
+    $sql = 'SELECT * FROM user WHERE deleted = 0';
+    $sql2 = 'SELECT * FROM user WHERE deleted = 1';
     $q = $db->query($sql);
+    $q2 = $db->query($sql2);
     $q->setFetchMode(PDO::FETCH_ASSOC);
-
+    $q2->setFetchMode(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     die("Could not connect to the database" . $e->getMessage());
 }
@@ -14,6 +16,7 @@ try {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,77 +28,152 @@ try {
     <title>Document</title>
     <style>
         body {
-          background: #dd5e89;
-          background: -webkit-linear-gradient(to left, #dd5e89, #f7bb97);
-          background: linear-gradient(to left, #dd5e89, #f7bb97);
-          min-height: 100vh;
-          color: black;
+            background: #dd5e89;
+            background: -webkit-linear-gradient(to left, #dd5e89, #f7bb97);
+            background: linear-gradient(to left, #dd5e89, #f7bb97);
+            min-height: 100vh;
+            color: black;
         }
     </style>
 </head>
+
 <body>
 
     <!-- Demo header-->
-    <section class="pb-5 header text-center">
-        <div class="container py-5 text-white">
-            <header class="py-5">
-                <h1 class="display-4">Liste des agents</h1>
-            </header>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-7 mx-auto">
-                    <div class="card border-0 shadow">
-                        <div class="card-body p-5">
-                            <!-- Responsive table -->
-                            <div class="table-responsive">
-                                <table class="table m-0">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">N°</th>
-                                            <th scope="col">Nom</th>
-                                            <th scope="col">Prénom</th>
-                                            <th scope="col">Email</th>
-                                            <th scope="col"></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                            while ($row = $q->fetch()):
-                                                ?>
-
-                                            <th scope="row"><?php echo htmlspecialchars($row['user_id']) ?></th>
-                                            <td><?php echo htmlspecialchars($row['name']) ?></td>
-                                            <td><?php echo htmlspecialchars($row['surname']) ?></td>
-                                            <td><?php echo htmlspecialchars($row['identifiant']) ?></td>
-
-                                        <?php
-                                            endwhile;
-                                                ?>
-                                            <td>
-                                                <!-- Call to action buttons -->
-                                                <ul class="list-inline m-0">
-                                                    <li class="list-inline-item">
-                                                        <button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></button>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button>
-                                                    </li>
-                                                </ul>
-                                            </td>
-                                    </tbody>
-                                </table>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 p-3">
+                <nav class="nav nav-tabs">
+                    <a class="nav-item nav-link active" href="#p1" data-toggle="tab">Agents enregistrés</a>
+                    <a class="nav-item nav-link" href="#p2" data-toggle="tab">Agents supprimés</a>
+                </nav>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="p1">
+                        <section class="pb-5 header text-center">
+                            <div class="container py-5 text-white">
+                                <header class="py-5">
+                                    <h1 class="display-4">Liste des agents</h1>
+                                </header>
                             </div>
-                        </div>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-7 mx-auto">
+                                        <div class="card border-0 shadow">
+                                            <div class="card-body p-5">
+                                                <!-- Responsive table -->
+                                                <div class="table-responsive">
+                                                    <table class="table m-0">
+                                                        <thead>
+                                                            <tr>
+                                                                <th scope="col">N°</th>
+                                                                <th scope="col">Nom</th>
+                                                                <th scope="col">Prénom</th>
+                                                                <th scope="col">Email</th>
+                                                                <th scope="col"></th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <?php
+                                                            while ($row = $q->fetch()) :
+                                                            ?>
+
+                                                                <th scope="row"><?php echo htmlspecialchars($row['user_id']) ?></th>
+                                                                <td><?php echo htmlspecialchars($row['name']) ?></td>
+                                                                <td><?php echo htmlspecialchars($row['surname']) ?></td>
+                                                                <td><?php echo htmlspecialchars($row['identifiant']) ?></td>
+                                                                <td>
+                                                                    <!-- Call to action buttons -->
+                                                                    <ul class="list-inline m-0">
+                                                                        <li class="list-inline-item">
+                                                                            <button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></button>
+                                                                        </li>
+                                                                        <li class="list-inline-item">
+                                                                            <a href="delete-agent-temp.php?id=<?php echo $row['user_id']; ?>"><button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button></a>
+                                                                        </li>
+                                                                    </ul>
+                                                                </td>
+                                                            <?php
+                                                            endwhile;
+                                                            ?>
+                                                        </tbody>
+                                                    </table>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <div class="tab-pane" id="p2">
+                        <section class="pb-5 header text-center">
+                            <div class="container py-5 text-white">
+                                <header class="py-5">
+                                    <h1 class="display-4">Liste des agents</h1>
+                                </header>
+                            </div>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-7 mx-auto">
+                                        <div class="card border-0 shadow">
+                                            <div class="card-body p-5">
+                                                <!-- Responsive table -->
+                                                <div class="table-responsive">
+                                                    <table class="table m-0">
+                                                        <thead>
+                                                            <tr>
+                                                                <th scope="col">N°</th>
+                                                                <th scope="col">Nom</th>
+                                                                <th scope="col">Prénom</th>
+                                                                <th scope="col">Email</th>
+                                                                <th scope="col"></th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <?php
+                                                            while ($row = $q2->fetch()) :
+                                                            ?>
+
+                                                                <tr>
+                                                                    <th scope="row"><?php echo htmlspecialchars($row['user_id']) ?></th>
+                                                                    <td><?php echo htmlspecialchars($row['name']) ?></td>
+                                                                    <td><?php echo htmlspecialchars($row['surname']) ?></td>
+                                                                    <td><?php echo htmlspecialchars($row['identifiant']) ?></td>
+                                                                    <td>
+                                                                        <!-- Call to action buttons -->
+                                                                        <ul class="list-inline m-0">
+                                                                            <li class="list-inline-item">
+                                                                                <button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></button>
+                                                                            </li>
+                                                                            <li class="list-inline-item">
+                                                                                <a href="delete-agent-def.php?id=<?php echo $row['user_id']; ?>"><button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button></a>
+                                                                            </li>
+                                                                        </ul>
+                                                                    </td>
+                                                                </tr>
+                                                            <?php
+                                                            endwhile;
+                                                            ?>
+                                                        </tbody>
+                                                    </table>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.0/umd/popper.min.js">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+
 </html>
